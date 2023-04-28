50f5a6be71212f78ccc123bfe37e35be7241d587
leilalinoo <aleila0921@gmail.com>
2023-04-28 12:33:48 +0200

    esemenyTrigger()

--- Elem.js
+++ Elem.js
@@ -5,11 +5,21 @@ class Elem {
     this.divElem = $(`article div:last-child`);
     this.pElem = this.divElem.children("p");
     this.divElem.on("click", () => {
-      this.setElem("X");
+      //this.setElem("X");
+      this.esemenyTrigger()
     });
   }
   setElem(ertek) {
     this.pElem.html(ertek);
+  }
+
+  //sajat esemeny - itt at tudj adni a példány magáról infokat
+
+  esemenyTrigger() {
+    const esemeny = new CustomEvent("elemKivalasztas", {
+      detail: this,
+    });
+    window.dispatchEvent(esemeny);
   }
 }
 
--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 import Elem from "./Elem.js";
 
 $(function () {
+    let lepes = 0;
   const szuloElem = $("article");
 
   for (let index = 0; index < 9; index++) {
-    const elem1 = new Elem(szuloElem);
+    const elem = new Elem(szuloElem);
   }
+
+
+  // a lepes páros, akkor X, ha páratlan, akkor O
+
+  $(window).on("elemKivalasztas", (event) => {
+    console.log(event.detail)
+  })
+
+
+
 });
